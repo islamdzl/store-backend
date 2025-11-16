e49ebea5cd3c3c4cebe801251107ee3a18d2db80
islamdz <legbedjislal@gmail.com>
2025-11-16 12:20:18 +0100

    edite connec-db

--- dist/config/connect-db.js
+++ dist/config/connect-db.js
@@ -5,15 +5,23 @@ const connect = async () => {
     if (!process.env.DATABASE_URL) {
         throw new SystemError('[.ENV] Invalid "DATABASE_URL"');
     }
-    await mongoose.connect(process.env.DATABASE_URL, {})
-        .then(() => logger.info('[DATABASE]: Connected Successfully'));
+    await mongoose.connect(process.env.DATABASE_URL, {
+        tls: true, // تفعيل TLS
+        tlsAllowInvalidCertificates: false, // لا تسمح بشهادات غير صالحة
+        tlsCAFile: '/etc/ssl/certs/ca-certificates.crt', // مسار الشهادات على Render
+    })
+        .then(() => logger.info('[DATABASE]: Connected Successfully'))
+        .catch(err => {
+        logger.error('[DATABASE]: Connection Failed', err);
+        process.exit(1);
+    });
 };
 mongoose.connection.on('disconnected', () => {
-    logger.warn('[DATABASE]: Connection is lost');
+    logger.warn('[DATABASE]: Connection lost, reconnecting...');
     connect();
 });
 mongoose.connection.on('reconnected', () => {
-    logger.warn('[DATABASE]: Reconection...');
+    logger.warn('[DATABASE]: Reconnected');
 });
 export default connect;
 //# sourceMappingURL=connect-db.js.map
