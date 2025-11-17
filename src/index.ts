7e6a42908250172bf652e97cdc24dd0d0f374f2b
islamdz <legbedjislal@gmail.com>
2025-11-17 19:38:44 +0100

    edite index

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,23 @@ import path from 'path'
 import logger from './shared/logger.js'
 import connectDb from './config/connect-db.js'
 import SystemError from './shared/system-error.js'
+import express from 'express'
 
-
-dotenv.config()
+dotenv.config({
+  path: path.join(process.cwd(), '.env')
+})
 
 const PORT = Number(process.env.PORT) || 2007;
 
 const main = async()=> {
   try{
+    
     const app = (await import('./app.js')).default;
+    logger.info(`Try running in port: ${PORT}`)
+
     app.listen(PORT, async ()=> {
-      await connectDb()
       logger.info(`Server listen in port: ${PORT}`)
+      await connectDb()
     })
 
   }catch(e: any) {
